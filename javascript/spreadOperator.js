e0a003a40f11d479c89543f9f5ee83627dfe7377
Rohini <rohini@frontm.com>
2020-12-06 01:55:30 +0530

    setting for graphql

---
+++ javascript/spreadOperator.js
@@ -0,0 +1,10 @@
+const userOne = {
+    name:`Davide`,
+    age:27,
+    city:`new york`,
+    country:`USA`
+}
+
+const userTwo = {...userOne,name:'Rohini',gender:'female'}
+console.log(userOne)
+console.log(userTwo)
