32905b10cdf20eecf0e7330906a47c3c38259ce2
Rohini <rohini@frontm.com>
2020-12-06 01:14:12 +0530

    Update asyncAwait.js

--- asyncAwait.js
+++ asyncAwait.js
@@ -1,16 +1,22 @@
 const displayMessage = (message) => {
     return new Promise((resolve, reject) => {
-        setTimeout(()=>{
-            if(message === `Wassup?`){
-                return reject(`Something went wrong`)
-            }
+        setTimeout(() => {
+            // if(message === `Wassup?`){
+            //     return reject(`Something went wrong`)
+            // }
             return resolve(message)
-        },3000)
+        }, 3000)
     })
 }
 
-const myFunc = async()=>{
-    try{
+const greeting = async () => {
+    return `Hey`
+}
+
+greeting().then(result => console.log(`****** ${result}`))
+
+const myFunc = async () => {
+    try {
         let result = ''
         result = await displayMessage(`Hello`)
         console.log(result)
@@ -18,8 +24,10 @@ const myFunc = async()=>{
         console.log(result)
         result = await displayMessage(`Wassup?`)
         console.log(result)
+        result = await greeting()
+        console.log(result)
     }
-    catch(error){
+    catch (error) {
         console.log(`Error : ${error}`)
     }
 }
