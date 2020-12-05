736cd4e49652fc6d991ae32c78fdec6c5533e82c
Rohini <rohini@frontm.com>
2020-12-06 01:04:05 +0530

    asyncAwait

---
+++ asyncAwait.js
@@ -0,0 +1,27 @@
+const displayMessage = (message) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(()=>{
+            if(message === `Wassup?`){
+                return reject(`Something went wrong`)
+            }
+            return resolve(message)
+        },3000)
+    })
+}
+
+const myFunc = async()=>{
+    try{
+        let result = ''
+        result = await displayMessage(`Hello`)
+        console.log(result)
+        result = await displayMessage(`There?`)
+        console.log(result)
+        result = await displayMessage(`Wassup?`)
+        console.log(result)
+    }
+    catch(error){
+        console.log(`Error : ${error}`)
+    }
+}
+
+myFunc()
